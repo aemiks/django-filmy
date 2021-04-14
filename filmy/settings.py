a3614644533f6e9a4cb92ec431e008aee7faa6e8
Krzysztof Kuta <atuk.krzysztof@gmail.com>
2021-04-14 17:52:23 +0200

    with correct settings

--- filmy/settings.py
+++ filmy/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'my_static')
-STATICFILES_DIRS = ['my_static']
+#STATICFILES_DIRS = ['my_static']
 
 
 MEDIA_URL = '/media/'
